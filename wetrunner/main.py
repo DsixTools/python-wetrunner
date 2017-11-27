f78a3ce5422b0c5660cd0da046ae3779eec61d11
Demerzel <xuanyou.pan@tum.de>
2017-11-27 10:57:42 +0100

    Replace main.py

--- wetrunner/main.py
+++ wetrunner/main.py
@@ -7,9 +7,9 @@ from collections import OrderedDict
 Cdictout = OrderedDict()
 
 for i in range(5):
-    for j in C_keys[i]:
-        C_values = [Cdict.get(key, 0) for key in j]
+    for keylist in C_keys[i]:
+        C_values = [Cdict.get(key, 0) for key in keylist]
         C_result = (M[U_keys[i][0]] + M[U_keys[i][1]]) @ C_values
-        for k in range(len(C_result)):
-            Cdictout[j[k]] = C_result[k]
+        for j in range(len(C_result)):
+            Cdictout[keylist[j]] = C_result[j]
         
