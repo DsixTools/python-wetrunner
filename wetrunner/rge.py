f1436b7fc9b680eb67bc4020f86f99b2a7615169
Demerzel <xuanyou.pan@tum.de>
2017-12-12 22:43:46 +0100

    Replace rge.py

--- wetrunner/rge.py
+++ wetrunner/rge.py
@@ -2,15 +2,15 @@ from wetrunner import evmat
 from wetrunner.definitions import C_keys, U_keys
 from collections import OrderedDict
 
-def C_out(C_in, Etas, Alphas, Alphaem, mb, mc, mtau):
+def C_out(C_in, Etas, Alphas, Alphaem, mb, mc, mtau, betas):
     Cdictout = OrderedDict()
     for i in range(5):
-        for j in C_keys[i]:
-            args = Etas, Alphas, Alphaem, mb, mc, mtau
+        for keylist in C_keys[i]:
+            args = Etas, Alphas, Alphaem, mb, mc, mtau, betas
             Us = getattr(evmat, U_keys[i][0])(*args)
             Ue = getattr(evmat, U_keys[i][1])(*args)
-            C_in = [C_in.get(key, 0) for key in j]
-            C_result = (Us + Ue) @ C_in
-            for k in range(len(C_result)):
-                Cdictout[j[k]] = C_result[k]
+            C_input = [C_in.get(key, 0) for key in keylist]
+            C_result = (Us + Ue) @ C_input
+            for j in range(len(C_result)):
+                Cdictout[keylist[j]] = C_result[j]
     return Cdictout
