3a60747c920f6d52bf8365557c1ca6745a5231c9
Jason Aebischer <jason.aebischer@tum.de>
2017-12-22 16:17:28 +0100

    added running alpha_s

--- wetrunner/qcd.py
+++ wetrunner/qcd.py
@@ -1,4 +1,27 @@
 import rundec
 
-def alpha_s(scale, n_flav):
-    return 0.11
+crd = rundec.CRunDec()
+
+def alpha_s(scale):
+    """3-loop cumputation of alpha_s for 5 and less flavours
+    with initial condition alpha_s(MZ) = 0.1185
+    or alpha_s(mc) = 0.505999      """
+    loop = 3
+    alphasMZ = 0.1185
+    MZ = 91.1876
+    mc = 1.2
+    alphamc = 0.505999
+    if scale > 4.18:
+        f = 5
+        return crd.AlphasExact(alphasMZ, MZ, scale, f, loop)
+    elif scale > 1.2:
+        f = 4
+        return crd.AlphasExact(alphasMZ, MZ, scale, f, loop)
+    elif scale > 0.95:
+        f = 3
+        return crd.AlphasExact(alphamc, mc, scale, f, loop)
+    elif scale > 0.48:
+        f = 2
+        return crd.AlphasExact(alphamc, mc, scale, f, loop)
+    else:
+        return "only one flavour"
