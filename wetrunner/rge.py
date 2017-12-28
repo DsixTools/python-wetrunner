c52038183794e9ef7e350618dcfe1d846bec62fa
David Straub <david.straub@tum.de>
2017-12-28 11:03:06 +0100

    Add neutrino operator running

--- wetrunner/rge.py
+++ wetrunner/rge.py
@@ -4,7 +4,7 @@ from collections import OrderedDict
 
 def C_out(C_in, Etas, Alphas, Alphaem, mb, mc, mtau, betas):
     Cdictout = OrderedDict()
-    for i in range(5):
+    for i in range(len(C_keys)):
         for keylist in C_keys[i]:
             args = Etas, Alphas, Alphaem, mb, mc, mtau, betas
             Us = getattr(evmat, U_keys[i][0])(*args)
